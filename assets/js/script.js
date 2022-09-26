74e68c0d6bffa810e1eb6fbcc7ce24fb94743d13
poggy-sl <poggy13poggy@gmail.com>
2022-09-26 16:23:50 +0300

    commit update

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// BURGER
+$('.header-menu__icon').click(function (event) {
+	$(this).toggleClass('active');
+	$('.header-menu').toggleClass('active');
+	if ($(this).hasClass('active')) {
+		$('body').data('scroll', $(window).scrollTop());
+	}
+	$('body').toggleClass('lock');
+	if (!$(this).hasClass('active')) {
+		$('body,html').scrollTop(parseInt($('body').data('scroll')));
+	}
+});
+// BURGER
+
+// IBG
+	function ibg() {
+		$.each($('.ibg'), function (index, val) {
+			if ($(this).find('img').length > 0) {
+				$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+			}
+		});
+	}
+	ibg();
+// IBG
+
+
+
+
